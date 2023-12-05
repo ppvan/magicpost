c64400a27996e9fe1093fd791ea511a9a7efac98
ppvan <phuclaplace@gmail.com>
2023-12-05 12:28:44 +0700

    feat: design database

--- magicpost/models.py
+++ magicpost/models.py
@@ -1,10 +1,87 @@
 from typing import Optional
+import enum
 
 from sqlmodel import Field, SQLModel
+from datetime import datetime
 
 
-class Hero(SQLModel, table=True):
+class MyBaseModel(SQLModel):
     id: Optional[int] = Field(default=None, primary_key=True)
+    created_at: datetime = Field(default=datetime.utcnow())
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+
+
+class PackageType(str, enum.Enum):
+    DOCUMENT = "DOCUMENT"
+    GOODS = "GOODS"
+
+
+class FailureType(str, enum.Enum):
+    RETURN = "RETURN"
+    CANCEL = "CANCEL"
+    CALL_SENDER = "CALL_SENDER"
+
+
+class Package(MyBaseModel, table=True):
+    # Sender
+    sender_name: str
+    sender_address: str
+    sender_phone: str
+    sender_zipcode: str
+
+    # Receiver
+    receiver_name: str
+    receiver_address: str
+    receiver_phone: str
+    receiver_zipcode: str
+    # Cash on delivery
+    cod: int
+    additional_cod: int
+    # Delivery Fees
+    weight: float
+    base_fee: int
+    additional_fee: int
+    type: PackageType
+    notes: Optional[str] = Field(default="")
+
+
+class OrderType(str, enum.Enum):
+    SERVICE_TO_HUB = "SERVICE_TO_HUB"
+    HUH_TO_HUB = "HUH_TO_HUB"
+    HUH_TO_SERVICE = "HUH_TO_SERVICE"
+
+
+class Order(MyBaseModel, table=True):
+    package_id: int
+    state: str
+    type: OrderType
+    sender_zipcode: str
+    receiver_zipcode: str
+
+
+class ServiceOffice(MyBaseModel, table=True):
     name: str
-    secret_name: str
-    age: Optional[int] = None
+    address: str
+    phone: str
+    zipcode: str
+
+
+class HubOffice(MyBaseModel, table=True):
+    name: str
+    address: str
+    phone: str
+    zipcode: str
+
+
+class Staff(MyBaseModel):
+    name: str
+    phone: str
+    manage_by: int
+
+
+class ServiceStaff(Staff, table=True):
+    pass
+
+
+class HubStaff(Staff, table=True):
+    pass
